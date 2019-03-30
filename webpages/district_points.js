3b0133e280d8a55c643b90d161c79ecc1fb0405f
Ari Meles-Braverman <arimb1999@gmail.com>
2019-03-30 18:31:21 +0300

    print hit

--- webpages/district_points.js
+++ webpages/district_points.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 });
 
 function reload(){
+	console.log("run")
 	$.ajax({
 		url: "https://arimb.ddns.net/getDistrictPoints/"+$("select#year").val(),
 		type: "GET",
 		success: function(result){
 			console.log(result)
+			console.log("success")
 		},
 		error: function(result){
 			console.log(result)
+			console.log("fail")
 		}
 	});
+	console.log("done")
 }
