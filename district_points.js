4a6a7bd4cbee1692743ca91a73972ac82bc36460
Ari MB <arimb1999@gmail.com>
2019-11-16 03:40:57 +0200

    first scroll header

--- district_points.js
+++ district_points.js
@@ -1,6 +1,23 @@
-
+var header, sticky;
 $(document).ready(function(){
+	header = $('thead')[0];
+	sticky = header.offsetTop;
+	window.onscroll = function(){
+		if (window.pageYOffset > sticky){
+			header.classList.add("sticky");
+		}else{
+			header.classList.remove("sticky");
+		}
+	}
+	$('select#year').change(update);
+	$('input#awards').change(update);
+	update();
+});
+
+function update(){
 	$('span#loading').css('display', 'block');
+	$('tbody').html('');
+	$('span#last-updated').html('');
 
 	var request = new XMLHttpRequest();
 	var url = 'https://arimb.pythonanywhere.com/district_points/' + $('select#year').children('option:selected')[0].value + ($('input#awards')[0].checked ? '_awards' : '');
@@ -11,10 +28,10 @@ $(document).ready(function(){
 			var row = $('tbody')[0].insertRow(-1);
 			row.insertCell(0).innerHTML = (i==0?1:data[i-1][1][1][0]==team[1][1][0]?'':i+1);
 			row.insertCell(1).innerHTML = team[0];
-			row.insertCell(2).innerHTML = team[1][1][0];
-			row.insertCell(3).innerHTML = team[1][1][1];
-			row.insertCell(4).innerHTML = team[1][1][2];
-			row.insertCell(5).innerHTML = team[1][1][3];
+			row.insertCell(2).innerHTML = team[1][1][0].toFixed(2);
+			row.insertCell(3).innerHTML = team[1][1][1].toFixed(2);
+			row.insertCell(4).innerHTML = team[1][1][2].toFixed(2);
+			row.insertCell(5).innerHTML = team[1][1][3].toFixed(2);
 			row.insertCell(6).innerHTML = team[1][1][4];
 		});
 		$('span#loading').css('display', 'none');
@@ -25,4 +42,4 @@ $(document).ready(function(){
 		console.log(err);
 	}
 	request.send();
-});
+}
