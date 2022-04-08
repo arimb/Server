cc864e28adc50d9b25373bafffa4a08035a16e77
AriMB <arimb1999@gmail.com>
2022-04-08 16:29:09 +0300

    alliance distribution

---
+++ alliance_distribution.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('select#year').change(update);
+	update();
+});
+
+function update(){
+	$('span#loading').css('display', 'block');
+	$('span#last-updated').html('');
+
+    var year = $('select#year').children('option:selected')[0].value;
+
+    $('div#images').html(
+        '<img src="https://arimb.pythonanywhere.com/alliance_distribution/' + year + '_Winners.png" alt="' + year + ` Winners" /> <br>
+        <img src="https://arimb.pythonanywhere.com/alliance_distribution/` + year + '_Finals.png" alt="' + year + ` Finals" /> <br>
+        <img src="https://arimb.pythonanywhere.com/alliance_distribution/` + year + '_Semifinals.png" alt="' + year + ` Semifinals" /> <br>
+        <img src="https://arimb.pythonanywhere.com/alliance_distribution/` + year + '_Quarterfinals.png" alt="' + year + ' Quarterfinals" />');
+	
+	$('span#loading').css('display', 'none');
+}
